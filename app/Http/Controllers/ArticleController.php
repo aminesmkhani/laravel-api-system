f3767c989d1463f819e96f7ae3d8e2f104e77f0d
Amin Esmkhani <amin.esmkhani75@gmail.com>
2022-02-09 19:15:15 +0330

    design route group & resource route!

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,9 +2,56 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    //
+
+    public function index()
+    {
+        $articles = Article::all();
+        return response()->json([
+           'data'   => $articles,
+            'meta'  => [
+                'count' => $articles->count()
+            ]
+        ],200);
+    }
+
+
+    public function create()
+    {
+        //
+    }
+
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function show($id)
+    {
+        //
+    }
+
+
+    public function edit($id)
+    {
+        //
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+
+    public function destroy($id)
+    {
+        //
+    }
 }
